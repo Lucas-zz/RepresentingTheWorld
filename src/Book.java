51806afdc7a344189a985d6babd79a9a3671ffb1
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-11 16:36:48 -0300

    feat: Book class implemented

---
+++ src/Book.java
@@ -0,0 +1,38 @@
+public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+    private int publishedYear;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    public void setPublishedYear(int year) {`
+        this.publishedYear = year;
+    }
+
+    public int getPublishedYear() {
+        return this.publishedYear;
+    }
+}
