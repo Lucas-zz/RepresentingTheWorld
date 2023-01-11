99d68d68f7fa508f58909124c41a3305f35d07f1
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-11 16:38:31 -0300

    feat: Class class implemented

---
+++ src/Class.java
@@ -0,0 +1,16 @@
+public class Class {
+    private String subject;
+    private Book[] books;
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public Book[] getBooks() {
+        return this.books;
+    }
+}
