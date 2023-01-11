11d8d41baf31a0626e9fdc9167e2c763ec17b203
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-11 16:36:29 -0300

    feat: Address class implemented

---
+++ src/Address.java
@@ -0,0 +1,47 @@
+public class Address {
+    private String state;
+    private String city;
+    private String district;
+    private String street;
+    private int number;
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setCity() {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDistrict() {
+        return this.district;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
